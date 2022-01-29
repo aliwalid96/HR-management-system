48a26ee29cae9b2a2e5633810b277d1f484dc459
ali muhsen <ali-waleed96@hotmail.com>
2022-01-29 18:42:02 +0200

    add acc page

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 function randomNumber(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function checkTheSalar(level){
+  if(level=="Senior"){
+      return randomNumber(1500,2000);
+  }else if(level=="Mid-Senior"){
+      return randomNumber(1000,1500);
+  }else if(level=="Junior"){
+   return randomNumber(500,1000);
+
   }
-
-  function checkTheSalar(level){
-    if(level=="Senior"){
-        return randomNumber(1500,2000);
-    }else if(level=="Mid-Senior"){
-        return randomNumber(1000,1500);
-    }else if(level=="Junior"){
-     return randomNumber(500,1000);
-
-    }
 }
 
 function randomIdNumber(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  let firstEmployeId=randomIdNumber(0,9999)
-  let secondEmployeId=randomIdNumber(0,9999)
-  let thirdEmployeId=randomIdNumber(0,9999)
-  let forthEmployeId=randomIdNumber(0,9999)
-  let fifthEmployeId=randomIdNumber(0,9999)
-  let sixthEmployeId=randomIdNumber(0,9999)
-  let seventhEmployeId=randomIdNumber(0,9999)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let firstEmployeId=randomIdNumber(0,9999)
+let secondEmployeId=randomIdNumber(0,9999)
+let thirdEmployeId=randomIdNumber(0,9999)
+let forthEmployeId=randomIdNumber(0,9999)
+let fifthEmployeId=randomIdNumber(0,9999)
+let sixthEmployeId=randomIdNumber(0,9999)
+let seventhEmployeId=randomIdNumber(0,9999)
 
 let firstEmployeSalary= checkTheSalar('Senior');
 let secondEmployeSalary= checkTheSalar('Senior');
@@ -33,7 +33,7 @@ let sexthEmployeSalary= checkTheSalar('Junior');
 let seventhEmployeSalary= checkTheSalar('Mid-Senior');
 
 function  calcualteTaxe(salary){
-    return salary*0.075;
+  return salary*0.075;
 }
 
 let firstEmployeNetSalary=firstEmployeSalary-calcualteTaxe(firstEmployeSalary);
@@ -47,14 +47,14 @@ let seventhEmployeNetSalary=seventhEmployeSalary-calcualteTaxe(seventhEmployeSal
 
 
 function TheConstructor(employeeID,fullName,department,level,salary,imgurl){
-    this.employeeID=employeeID;
-    this.fullName=fullName;
-    this.department=department;
-    this.level=level;
-    this.salary=salary;
-    this.imgurl=imgurl;
+  this.employeeID=employeeID;
+  this.fullName=fullName;
+  this.department=department;
+  this.level=level;
+  this.salary=salary;
+  this.imgurl=imgurl;
 
-    TheConstructor.allEmployee.push(this);
+  TheConstructor.allEmployee.push(this);
 
 }
 
@@ -62,13 +62,36 @@ TheConstructor.allEmployee=[];
 
 
 
+let employeeForm= document.getElementById('theEmployeeForm');
+employeeForm.addEventListener('submit', addNewEmployee);
+
+function addNewEmployee(event){
+event.preventDefault();
+
+let fullName = event.target.fullName.value;
+let employeeId = event.target.name.value;  
+let imgUrl=event.target.imgUrl.value;
+let department=event.target.departure.value;
+let level=event.target.level.value;
+let salary= checkTheSalar(level);
+
+
+
+let newEmployee = new TheConstructor(employeeId,fullName,department,level,salary,imgUrl);
+settingItem();
+render();
+
+}
+
+
+
 function render(){
-   // document.write(`<tr><td> ${this.fullName}</td> <td> ${this.salary}</td></tr>`);
-   let theSection=document.getElementById('employee');
-   for(i=0;i<TheConstructor.allEmployee.length;i++){
+ // document.write(`<tr><td> ${this.fullName}</td> <td> ${this.salary}</td></tr>`);
+ let theSection=document.getElementById('employee');
+ for(i=0;i<TheConstructor.allEmployee.length;i++){
 let theEmployee=TheConstructor.allEmployee[i];
 
-  
+
 let divEl=document.createElement('div');
 theSection.appendChild(divEl);
 
@@ -77,27 +100,28 @@ divEl.appendChild(imgEl);
 imgEl.setAttribute('src', `./${theEmployee.imgurl}.jpg`|| theEmployee.imgurl);
 imgEl.setAttribute('alt',theEmployee.name);
 
- let pEmpId=document.createElement('p')
- divEl.appendChild(pEmpId);
- pEmpId.textContent=theEmployee.employeeID;
+let pEmpId=document.createElement('p')
+divEl.appendChild(pEmpId);
+pEmpId.textContent=theEmployee.employeeID;
 
- let pEmpName=document.createElement('p');
- divEl.appendChild(pEmpName);
- pEmpName.textContent=`Name : ${theEmployee.fullName}`;
+let pEmpName=document.createElement('p');
+divEl.appendChild(pEmpName);
+pEmpName.textContent=`Name : ${theEmployee.fullName}`;
 
- let selectDept=document.createElement('p');
- divEl.appendChild(selectDept);
- selectDept.textContent=`department ${theEmployee.department}`;
+let selectDept=document.createElement('p');
+divEl.appendChild(selectDept);
+selectDept.textContent=`department ${theEmployee.department}`;
 
- let selectlevel=document.createElement('p');
- divEl.appendChild(selectlevel);
- selectlevel.textContent=`level ${theEmployee.level}`;
+let selectlevel=document.createElement('p');
+divEl.appendChild(selectlevel);
+selectlevel.textContent=`level ${theEmployee.level}`;
 
 
 
- let pSalary=document.createElement('p');
+let pSalary=document.createElement('p');
 divEl.appendChild(pSalary);
 pSalary.textContent=theEmployee.salary;
+
 }
 
 
@@ -114,52 +138,36 @@ let fifthEmploye= new TheConstructor (fifthEmployeId,"OmarZaid","Development	","
 let sixthEmploye= new TheConstructor (sixthEmployeId,"Rana Saleh","Development","Junior",sexthEmployeNetSalary,"Rana");
 let seventhEmploye= new TheConstructor (seventhEmployeId,"Hadi","Finance","Mid-Senior",seventhEmployeNetSalary,"Hadi");
 
+
+
+
 render();
-
-let employeeForm= document.getElementById('theEmployeeForm');
-employeeForm.addEventListener('submit', addNewEmployee);
-
-function addNewEmployee(event){
-  event.preventDefault();
-  
-  let fullName = event.target.fullName.value;
-  let employeeId = event.target.name.value;  
-  let imgUrl=event.target.imgUrl.value;
-  let department=event.target.departure.value;
-  let level=event.target.level.value;
-  let salary= checkTheSalar(level);
-
-
-
-  let newEmployee = new TheConstructor (employeeId,fullName,department,level,salary,imgUrl);
-
-  
-render();
-}
-
 
 
 function settingItem(){
-  let data = JSON.stringify(TheConstructor.allEmployee);// convert the data to JSON format 
-  localStorage.setItem('employeeD',data)
+let data = JSON.stringify(TheConstructor.allEmployee);// convert the data to JSON format 
+localStorage.setItem('employeeD',data)
 
 
 }
+
+
 
 
 //2. get date : take the data from local storage 
 
 function gettingItem(){
-  let stringObj = localStorage.getItem('employeeD');
-  // console.log(stringObj);
-  let parsObj = JSON.parse(stringObj);
-  // console.log(parsObj);
-  if (parsObj !== null){
-    TheConstructor.allEmployeep = parsObj;
-    
-  }
-
+let stringObj = localStorage.getItem('employeeD');
+ //console.log(stringObj);
+let parsObj = JSON.parse(stringObj);
+ // console.log(parsObj);
+if (parsObj !== null){
+  TheConstructor.allEmployeep = parsObj;
   
+}
+
+render();
+
 
 }
 
